f78e33529a7b6036cb9c8c7a509db60773a2cb1b
Ricardo Mélida <ricardomelida92@gmail.com>
2020-02-22 09:19:13 -0300

    Nuevos diseño y esilos de repositorio

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+var btnMenu= document.querySelector("#icono-menu");
+    
+btnMenu.addEventListener("change", verMenu, false);
+
+
+function verMenu(){
+    
+  var seleciona = btnMenu.checked;
+  var menu = $('#menu');
+  var content = $('#contenedor');
+  
+  if(seleciona){
+    
+    $('header').animate({marginLeft: '80%'});
+    menu.animate({marginLeft: '0px'});
+    content.addClass('mover-content');
+  }else {
+    
+    content.removeClass('mover-content');
+    $('header').animate({marginLeft: '0%'});
+    menu.animate({marginLeft: '-80%'});
+    //menú.style.display = 'inherit';
+  }  
+}
+
+
+});
+
+
