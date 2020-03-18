60e098c90b367784f8b2886af712c0bd85a04df4
Sub Estacion <subestacion@paseolagaleria.com>
2020-03-18 07:47:55 -0300

    Se agrega item de cada pagina

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,46 @@
 $(document).ready(function() {
+
+  var btnMenu= document.querySelector("#icono-menu");
+  
+  btnMenu.addEventListener("change", verMenu, false);
+  
   var tamaho = screen.width;
+  var titulo = document.title;
+  var selec = $('#selec-item');
+  var index = $('ul li:nth-child(1)');
+
+  switch(titulo) {
+    
+    case 'Ricardo M.':
+      $('ul li:nth-child(1)').attr('id', 'selec-item');
+      break;
+      
+    case 'Habilidades':
+      $('ul li:nth-child(4)').attr('id', 'selec-item');
+      break;
+        
+    case 'Sobre Mi':
+      $('ul li:nth-child(2)').attr('id', 'selec-item');
+      break;
+          
+    case 'Portafolio':
+      $('ul li:nth-child(3)').attr('id', 'selec-item');
+      break;
+            
+    case 'Contacto':
+        $('ul li:nth-child(5)').attr('id', 'selec-item');
+        break;
+
+  }
+
+  a.attr('id', 'selec-item');
   
   if(tamaho < 1250) {
     $('#menu').animate({marginLeft: '-80%'});
-    s
+    
   }
 
-  var btnMenu= document.querySelector("#icono-menu");
 
-  btnMenu.addEventListener("change", verMenu, false);
 
 
   function verMenu(){
