ed7a2357d347e1404e5e7e42a081e3ad627fb6f1
Ricardo Melida <ricardomelida92@gmail.com>
2020-03-22 16:04:31 -0400

    Se modifica proyecto

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,64 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  var btnMenu= document.querySelector("#icono-menu");
-  
+  var btnMenu = document.querySelector("#icono-menu");
+  var miscroll = $(window).scrollTop();
+
   btnMenu.addEventListener("change", verMenu, false);
-  
+  //window.addEventListener('scroll', verMenu, false);
+
   var tamaho = screen.width;
   var titulo = document.title;
 
-  switch(titulo) {
+  switch (titulo) {
     case 'Ricardo M.':
       $('ul li:nth-child(1)').attr('id', 'selec-item');
       break;
-      
+
     case 'Habilidades':
       $('ul li:nth-child(4)').attr('id', 'selec-item');
       break;
-        
+
     case 'Sobre Mi':
       $('ul li:nth-child(2)').attr('id', 'selec-item');
       break;
-          
+
     case 'Portafolio':
       $('ul li:nth-child(3)').attr('id', 'selec-item');
       break;
-            
+
     case 'Contacto':
-        $('ul li:nth-child(5)').attr('id', 'selec-item');
-        break;
+      $('ul li:nth-child(5)').attr('id', 'selec-item');
+      break;
 
   }
-  
-  if(tamaho < 1250) {
-    $('#menu').animate({marginLeft: '-80%'});
-    
+
+  if (tamaho < 1250) {
+    $('#menu').animate({ marginLeft: '-80%' });
+
   }
 
-  function verMenu(){
-      
-    //var tamaho = screen.width;
+  function verMenu() {
 
     var seleciona = btnMenu.checked;
     var menu = $('#menu');
     var content = $('#contenedor');
 
-      if(seleciona){
-        $('header').animate({marginLeft: '80%'});
-        menu.animate({marginLeft: '0px'});
-        content.addClass('mover-content');
-      }else {
-        content.removeClass('mover-content');
-        $('header').animate({marginLeft: '0%'});
-        menu.animate({marginLeft: '-80%'});
-        //menú.style.display = 'inherit';
-      }  
-  }
-  
-});
+    if (seleciona) {
+      $('header').animate({ marginLeft: '80%' });
+      menu.animate({ marginLeft: '0px' });
+      content.addClass('mover-content');
 
+    } else{
+
+        content.removeClass('mover-content');
+        $('header').animate({ marginLeft: '0%' });
+        menu.animate({ marginLeft: '-80%' });
+        console.log(miscroll);
+
+      
+    }
+
+  }
+
+});
 
