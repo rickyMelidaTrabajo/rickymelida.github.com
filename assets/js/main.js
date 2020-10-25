562dfa792c7afe1bf1dd016dd5dbac335311ad71
Ricky Melida <ricardomelida92.trabajo@gmail.com>
2020-10-25 20:40:51 -0300

    Se modifica estilos y paginas LG
    Se han arreglado errores en el diseño de las distintas paginas
    asi como tambien solucionar el error de la pagina de sobre mi
    donde se desbordaba a los costados.
    Faltaria poder solucionar lo que seria los proyectos y poder dar
    mas detalle de todo eso.

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
       break;
 
     case 'Portafolio': 
-      $('ul li:nth-child(2)').attr('id', 'selec-item');
+      $('ul li:nth-child(3)').attr('id', 'selec-item');
       break;
 
     case 'Contacto':
@@ -92,17 +92,36 @@ $(document).ready(function () {
     contenedor.appendChild(project2);
   }
 
-  let namesProjects = ['Ventas', 'Tareas Electricas', 'Admin Electricas', 'Ricky Web'];
 
 
-  
   /*
-     Aqui seleccionamos la imagen y nombre del proyecto y le cambiamos dichos atributos
+    Aqui seleccionamos la imagen y nombre del proyecto y le cambiamos dichos atributos
   */
-  for(let j=1; j <=4; j++) {
-    document.querySelector(`#project_${j} img`).setAttribute('src', `../assets/images/projects/project_${j+1}.jpg`);
-    document.querySelector(`#project_${j} .name-project a`).innerHTML = namesProjects[j-1];
-  }
 
+  // Proyecto 2
+  let imagen = document.querySelector('#project_1 img');
+  let tituloProject = document.querySelector('#project_1 .name-project a');
+  imagen.setAttribute('src', `../assets/images/projects/ventas/1.jpg`)
+  tituloProject.innerHTML = "Ventas";
+
+
+  // Proyecto 3
+  let imagen1 = document.querySelector('#project_2 img');
+  let tituloProject1 = document.querySelector('#project_2 .name-project a');
+  imagen1.setAttribute('src', `../assets/images/projects/tareas_electrica/1.jpg`);
+  tituloProject1.innerHTML = 'Tareas Electricas';
+  
+  
+  // Proyecto 4
+  let imagen2 = document.querySelector('#project_3 img');
+  let tituloProject2 = document.querySelector('#project_3 .name-project a');
+  imagen2.setAttribute('src', `../assets/images/projects/admin_electrica/1.jpeg`);
+  tituloProject2.innerHTML = 'Admin Electricas';
+
+  // Proyecto 5
+  let imagen3 = document.querySelector('#project_4 img');
+  let tituloProject3 = document.querySelector('#project_4 .name-project a');
+  imagen3.setAttribute('src', `../assets/images/projects/ricky_web/1.jpg`);
+  tituloProject3.innerHTML = 'Ricky Web';
 
 });
---
+++ assets/js/slider.js
@@ -0,0 +1,42 @@
+var imagen = $("#img_1");
+
+
+$("#slider_sec").css("margin-left", "-" + 100 + "%");
+
+/*Va a la Derecha*/
+$("input[name=Derecha]").click(function () {
+    $("#slider_sec").animate({ marginLeft: 0 + "%" }, 700, function () {
+        $("#slider_sec .img:last").insertBefore("#slider_sec .img:first");
+        $("#slider_sec").css("margin-left", "-" + 100 + "%");
+    });
+});
+
+/*Va a la Izquierda*/
+$("input[name=Izquierda]").click(function () {
+    $("#slider_sec").animate({ marginLeft: "-" + 200 + "%" }, 700, function () {
+        $("#slider_sec .img:first").insertAfter("#slider_sec .img:last");
+        $("#slider_sec").css("margin-left", "-" + 100 + "%");
+    });
+});
+
+setInterval(function () {
+    $("#slider_sec").animate({ marginLeft: 0 + "%" }, 1000, function () {
+        $("#slider_sec .img:last").insertBefore("#slider_sec .img:first");
+        $("#slider_sec").css("margin-left", "-" + 100 + "%");
+    });
+}, 4000);
+
+
+/*
+    Una versión muy útil de git log esla siguiente, pues nos permite veren que lugares está
+master y HEAD, entre otras cosas:
+$ git log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short
+
+
+Como estas órdenes son demasiado largas,Git nos permite crear alias para crear nuevas
+órdenes parametrizadas. Para ello editaremos un archivo llamado
+.gitconfig que está en nuestro $HOME y le añadiremos estas líneas al final:
+[alias]
+hist = log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short
+
+*/
